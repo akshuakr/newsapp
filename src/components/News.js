9451eb0ad0c036351c4c1c061c67eed8ff5edf61
akshu <in.akr.2000@gmail.com>
2023-03-19 03:03:57 +0530

    v28_1

--- src/components/News.js
+++ src/components/News.js
@@ -14,7 +14,7 @@ export default class News extends Component {
 
     async componentDidMount() {
         let url =
-            "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=84b2994e717c4887b432c080ef02dfa5&page=1&pageSize=8";
+            "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=46647463e0fe47a89d0e97d6c4439b46&page=1&pageSize=8";
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults,  });
@@ -24,7 +24,7 @@ export default class News extends Component {
 
     handlePrevClick = async () =>{
         let url =
-            `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=84b2994e717c4887b432c080ef02dfa5&page=${this.state.page -1}&pageSize=8`;
+            `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=46647463e0fe47a89d0e97d6c4439b46&page=${this.state.page -1}&pageSize=8`;
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({ articles: parsedData.articles, page: this.state.page -1 });
@@ -33,8 +33,8 @@ export default class News extends Component {
     handleNextClick = async () =>{
 
         if(this.state.page + 1 <= Math.ceil(this.state.totalResults/8)){
-        // console.log(this.state.page);
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=84b2994e717c4887b432c080ef02dfa5&page=${
+        console.log("before " + this.state.page);
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=46647463e0fe47a89d0e97d6c4439b46&page=${
             this.state.page + 1
         }&pageSize=8`;
         let data = await fetch(url);
@@ -44,7 +44,8 @@ export default class News extends Component {
             page: this.state.page + 1,
         })}
         
-        
+        console.log(this.state.totalResults / 8);
+        console.log(Math.ceil(this.state.totalResults / 8));
     }
 
     
@@ -59,11 +60,7 @@ export default class News extends Component {
                         return (
                             <div className="col-md-3" key={element.url}>
                                 <NewsItem
-                                    title={
-                                        element.title
-                                            ? element.title
-                                            : ""
-                                    }
+                                    title={element.title ? element.title : ""}
                                     description={
                                         element.description
                                             ? element.description.slice(0, 88)
@@ -90,9 +87,10 @@ export default class News extends Component {
                     </button>
                     <button
                         type="button"
-                        // disabled={
-                        //     this.state.page - 1 > Math.ceil((this.totalResults) / 8)
-                        // }
+                        disabled={
+                            this.state.page + 1 >
+                            Math.ceil(this.state.totalResults / 8)
+                        }
                         className="btn btn-dark"
                         onClick={this.handleNextClick}>
                         Next &#8680;
